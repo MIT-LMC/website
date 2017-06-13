2cd2c94c10577794159a9358acef061efe190009
atitus5 <drewtitus59@gmail.com>
2017-06-12 23:04:12 -0400

    Added view skeletons; started About Us view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET Home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'MIT LMC | Live Music Connection', activeView: 'home' });
+});
+
+/* GET About Us page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'MIT LMC | About LMC', activeView: 'about' });
+});
+
+/* GET Concerts page. */
+router.get('/concerts', function(req, res, next) {
+  res.render('concerts', { title: 'MIT LMC | Concert Series', activeView: 'concerts' });
+});
+
+/* GET Open Source page. */
+router.get('/opensource', function(req, res, next) {
+  res.render('opensource', { title: 'MIT LMC | Open Source', activeView: 'opensource' });
+});
+
+/* GET Rentals page. */
+router.get('/rentals', function(req, res, next) {
+  res.render('rentals', { title: 'MIT LMC | Equipment Rentals', activeView: 'rentals' });
+});
+
+/* GET Sponsorship page. */
+router.get('/sponsors', function(req, res, next) {
+  res.render('sponsors', { title: 'MIT LMC | Sponsorship', activeView: 'sponsors' });
+});
+
+/* GET Contact Us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'MIT LMC | Contact Us', activeView: 'contact' });
 });
 
 module.exports = router;
